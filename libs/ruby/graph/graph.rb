7dedf31d24a88358e7d9fd3c484d7faa46266f32
王若愚 <wrymax@qq.com>
2017-06-14 00:36:46 +0800

    add BFS and DFS for Graph

--- libs/ruby/graph/graph.rb
+++ libs/ruby/graph/graph.rb
@@ -15,6 +15,7 @@ class AdjacentList
   def initialize(head_node=nil)
     self.head_node = head_node
   end
+
 end
 
 
@@ -49,6 +50,51 @@ class Graph
     self
   end
 
+  # Breadth-First Search
+  def bfs(node_name, &block)
+    # use a queue to save nodes to be operated
+    queue = Queue.new
+
+    # use a visited array to store visit status
+    visited = Array.new(self.adj_list_array.length, 0)
+
+    # start with a node
+    index = self.dict[node_name]
+    adj_list = self.adj_list_array[index]
+    node = adj_list.head_node
+
+    # operate node first
+    push_node_to_queue(queue, node, visited)
+    
+    while !queue.empty? do
+      current_node = queue.pop
+
+      if block_given?
+        yield current_node 
+      else
+        puts "Node #{current_node.value} is operated!"
+      end
+
+      adj_nodes = adjacent_nodes(current_node.value)
+      adj_nodes.each do |n|
+        push_node_to_queue(queue, n, visited) unless visited?(visited, n)
+      end
+    end
+
+    puts "Breadth-First Search ends."
+  end
+
+  # Depth-First Search
+  def dfs(node_name, &block)
+    # use a visited array to store visit status
+    visited = Array.new(self.adj_list_array.length, 0)
+
+    # start recursion
+    dfs_loop(node_name, visited, &block)
+
+    puts "Depth-Frist Search ends."
+  end
+
   def print
     self.adj_list_array.each do |list|
       node = list.head_node
@@ -80,6 +126,7 @@ class Graph
     data
   end
 
+  # build Graph from scratch
   def build_from_data(data)
     data.each do |list_data|
       adjacent_list = AdjacentList.new
@@ -97,6 +144,53 @@ class Graph
         node2
       end
       self.adj_list_array.push(adjacent_list)
+    end
+  end
+
+  def adjacent_nodes(node_name)
+    result = []
+    index = self.dict[node_name]
+    return nil unless index
+
+    adj_list = self.adj_list_array[index]
+
+    node = adj_list.head_node
+    next_node = node.next_node
+    while next_node do
+      result.push(next_node)
+      next_node = next_node.next_node
+    end
+
+    result
+  end
+
+  def push_node_to_queue(queue, node, visited)
+    queue.push(node)
+    visited[self.dict[node.value]] = 1
+  end
+
+  def visited?(visited, node)
+    visited[self.dict[node.value]] == 1
+  end
+
+  def dfs_loop(node_name, visited, &block)
+    # mark as visited
+    index = self.dict[node_name]
+    adj_nodes = adjacent_nodes(node_name)
+    node = self.adj_list_array[index].head_node
+
+    visited[index] = 1
+    if block_given?
+      yield node
+    else
+      puts "#{node.value} is visited."
+    end
+
+    adj_nodes.each do |adj_node|
+      adj_node_index = self.dict[adj_node.value]
+      # puts "adj_node_index = #{adj_node_index}; node = #{adj_node.value}"
+      # puts "visited[adj_node_index] = #{visited[adj_node_index]}"
+      dfs_loop(adj_node.value, visited, &block) if visited[adj_node_index] == 0
     end
   end
 end
