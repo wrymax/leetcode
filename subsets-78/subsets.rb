61e064626caf883071b3ee3c954989e9aeb5df40
王若愚 <wrymax@qq.com>
2018-01-05 21:43:21 +0800

    uncompleted

---
+++ subsets-78/subsets.rb
@@ -0,0 +1,20 @@
+# Brute force solution
+class Subset
+  attr_accessor :nums
+
+  def initialize(nums)
+    self.nums = nums
+  end
+
+  # Consider elements as a graph, in which all elements are connected
+  # Target is to find all possible combinations(paths) from any starting point to any endpoint
+  def recursive_solution
+
+  end
+
+  private
+
+  # DFS to get all permutations
+  def dfs_helper
+  end
+end
