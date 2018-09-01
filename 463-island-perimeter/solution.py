a164507064a0d9508f8f32fbce52cfab761bd29d
wrymax <wrymax@qq.com>
2018-09-01 16:26:25 -0400

    update 463

--- 463-island-perimeter/solution.py
+++ 463-island-perimeter/solution.py
@@ -4,25 +4,13 @@ class Solution:
         :type grid: List[List[int]]
         :rtype: int
         """
-        # BFS + count perimeter with number of 1 around each 1
-        if len(grid) == 0:
-            return 0
-        rows = len(grid)
-        cols = len(grid[0])
-        
         perimeter = 0
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    temp_p = 4
-                    for i in [(r, c+1), (r, c-1), (r+1, c), (r-1, c)]:                       
-                        if i[0] >= 0 and i[0] < rows and i[1] >= 0 and i[1] < cols and grid[i[0]][i[1]] == 1:
-                            temp_p -= 1
-                    perimeter += temp_p
+        for r, row in enumerate(grid):
+            for c, value in enumerate(row):
+                if value:
+                    perimeter += 4
+                    if r > 0 and grid[r-1][c] == 1: perimeter -= 2
+                    if c > 0 and grid[r][c-1] == 1: perimeter -= 2
                     
         return perimeter
-
-s = Solution()
-grid = [[1,1],[1,1]]
-print(s.islandPerimeter(grid))
---
+++ 463-island-perimeter/test.py
@@ -0,0 +1,5 @@
+from Solution import Solution
+
+s = Solution()
+grid = [[1,1],[1,1]]
+print(s.islandPerimeter(grid))
